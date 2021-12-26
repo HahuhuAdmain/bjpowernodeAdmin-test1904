883388766dee10996f2b375bc76cfdedaf98f48d
hwj <hwj@qq.com>
2021-12-26 08:53:59 +0800

    123

---
+++ src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.domain;
+
+/**
+ * @Auther: Huwenjun
+ * @Date: 2021/12/25 - 12 - 25 - 17:19
+ * @Description: com.bjpowernode.domain
+ * @version: 1.0
+ */
+public class Person {
+    private  String id;
+
+}
